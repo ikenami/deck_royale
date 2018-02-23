88270e303624a35bfa856bd583ab9dbd31226681
Ikenami <uskyjp@gmail.com>
2018-02-23 14:38:06 -0300

    Going back to request API using HTTP instead of HTTPS

--- src/components/arenas/AboutArenas.vue
+++ src/components/arenas/AboutArenas.vue
@@ -6,10 +6,10 @@
     <p class="info-text">Need at least <b>{{ arena.minTrophies }} Trophies</b> </p>
     <p class="info-text"><b>Can Donate</b></p>
     <p class="info-text sub-text"><b>{{ arena.clan.donate.common}} Common <span v-if="(arena.clan.donate.common == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
-    <p class="info-text sub-text"><b>{{ arena.clan.donate.rare }} Rare <span v-if="(arena.clan.donate.rare == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
+    <p class="info-text sub-text"><b>{{ arena.clan.donate.rare }} Rare <span v-if="(arena.clan.donate.rare  == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
     <p class="info-text"><b>Can Request</b></p>
     <p class="info-text sub-text"><b>{{ arena.clan.request.common}} Common <span v-if="(arena.clan.request.common == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
-    <p class="info-text sub-text"><b>{{ arena.clan.request.rare }} Rare <span v-if="(arena.clan.request.rare == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
+    <p class="info-text sub-text"><b>{{ arena.clan.request.rare }} Rare <span v-if="(arena.clan.request.rare  == '1')">Card</span><span v-else>Cards</span></b> to Clan</p>
   </div>
 </template>
 <script>
